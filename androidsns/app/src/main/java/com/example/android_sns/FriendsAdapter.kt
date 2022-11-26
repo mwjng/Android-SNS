2445b3ce2fe58c4e90ea295fd0ae91f4d88fd22f
jiyoonK <wldbs068@gmail.com>
2022-11-26 16:26:54 +0900

    게시물 이미지 포함 작성 게시물 이미지 포함 보여줌 친구 프로필, 게시물 이미지 확인

--- androidsns/app/src/main/java/com/example/android_sns/FriendsAdapter.kt
+++ androidsns/app/src/main/java/com/example/android_sns/FriendsAdapter.kt
@@ -2,11 +2,13 @@ package com.example.android_sns
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.startActivity
@@ -16,35 +18,45 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 
 class FriendsAdapter (val context: Context?, val UserList: ArrayList<Friend>) : BaseAdapter(){
 
-        override fun getCount(): Int {
-            return UserList.size
-        }
-        override fun getItem(count: Int): Any {
-            return UserList[count]
-        }
-        override fun getItemId(i: Int): Long {
-            return 0
+    override fun getCount(): Int {
+        return UserList.size
+    }
+    override fun getItem(count: Int): Any {
+        return UserList[count]
+    }
+    override fun getItemId(i: Int): Long {
+        return 0
+    }
+
+    override fun getView(count: Int, convertView: View?, parent: ViewGroup?): View {
+        val db: FirebaseFirestore = Firebase.firestore
+        val itemsCollectionRef = db.collection("users")
+        val view : View = LayoutInflater.from(parent?.context).inflate(R.layout.friends, null)
+        val proImg = view.findViewById<ImageView>(R.id.imageViewf)
+        val name = view.findViewById<TextView>(R.id.friendname)
+        val user = UserList[count]
+        name.text = user.nickname
+        // 친구 프사
+        //proImg.setImageBitmap(???)
+        val imgRef = Firebase.storage.reference.child("images/${user.email}PROFILE")
+        imgRef?.getBytes(Long.MAX_VALUE)?.addOnSuccessListener {
+            val bmp = BitmapFactory.decodeByteArray(it, 0, it.size)
+            proImg.setImageBitmap(bmp)
         }
 
-        override fun getView(count: Int, convertView: View?, parent: ViewGroup?): View {
-            val db: FirebaseFirestore = Firebase.firestore
-            val itemsCollectionRef = db.collection("users")
-            val view : View = LayoutInflater.from(parent?.context).inflate(R.layout.friends, null)
-            val name = view.findViewById<TextView>(R.id.friendname)
-            val user = UserList[count]
-            name.text = user.nickname
 
-            view.findViewById<Button>(R.id.fprofile).setOnClickListener {
-                val intent = Intent(context, FriendProfile::class.java)
-                intent.putExtra("key",user.email)
-                startActivity(view.context,intent,null)
-            }
-
-            return view
+        view.findViewById<Button>(R.id.fprofile).setOnClickListener {
+            val intent = Intent(context, FriendProfile::class.java)
+            intent.putExtra("key",user.email)
+            startActivity(view.context,intent,null)
         }
+
+        return view
+    }
 
 }
--- androidsns/app/src/main/java/com/example/android_sns/WriteActivity.kt
+++ androidsns/app/src/main/java/com/example/android_sns/WriteActivity.kt
@@ -1,25 +1,32 @@
 package com.example.android_sns
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import com.example.android_sns.databinding.ActivityWriteBinding
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class WriteActivity : AppCompatActivity(){
     val db: FirebaseFirestore = Firebase.firestore
+    val storage = Firebase.storage
     val email = Firebase.auth.currentUser?.email.toString()
     val itemsCollectionRef = db.collection("users")
     var name = ""
+    var imgUri=""
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +43,14 @@ class WriteActivity : AppCompatActivity(){
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
+        val img = binding.userImage
+        val readImg = registerForActivityResult(ActivityResultContracts.GetContent()) {
+            img.setImageURI(it)
+            imgUri = it.toString()
+        }
+        binding.uploadIMG.setOnClickListener {
+            readImg.launch("image/*")
+        }
 
         binding.conf.setOnClickListener {
             val current = LocalDateTime.now()
@@ -46,6 +61,8 @@ class WriteActivity : AppCompatActivity(){
             val nickname = name
             val title = binding.editTitle.text
             val content = binding.editText.text
+
+
             val like = 0
             val date = formatted
             val itemMap = hashMapOf(
@@ -58,11 +75,18 @@ class WriteActivity : AppCompatActivity(){
                 "like" to like,
                 "date" to date.toString()
             )
+            //db에 upload 저장
             db.collection("users").document(email).collection("upload").document(formatted2)
                 .set(itemMap)
                 .addOnSuccessListener {  }
                 .addOnFailureListener {  }
-
+            // storage에 이메일_date로 저장
+            val imgRef = storage.reference.child("images/${email}_${date}")
+            if (imgUri.toUri() != null) {
+                imgRef.putFile(imgUri.toUri()).addOnCompleteListener{
+                    //Toast.makeText(context, "업로드 대따", Toast.LENGTH_SHORT).show();
+                }
+            }
             startActivity(Intent(this, MainActivity::class.java))
         }
     }
